b25ab60674016753aee0a722c61d3010cff7255d
vaebe <18137693952@163.com>
2024-05-06 22:50:26 +0800

    feat: 增加 ImportMetaEnv 类型声明

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -10,3 +10,14 @@ declare module 'element-plus/dist/locale/zh-cn.mjs' {
   const zhCn: any
   export default zhCn
 }
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_APP_AXIOS_TIMEOUT: string
+  readonly VITE_APP_BASE_URL: string
+  readonly VITE_APP_WS_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
