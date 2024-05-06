32d0af13d27973236ebd1bfe752d8155b7643658
vaebe <18137693952@163.com>
2024-05-06 23:05:36 +0800

    chore: 增加包体积分析插件

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +15,8 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
-    })
+    }),
+    visualizer(),
   ],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }]
