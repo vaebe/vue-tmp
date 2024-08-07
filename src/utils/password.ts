c46bfc517a13d2b61b5760dcf5efe084529bc27b
vaebe <18137693952@163.com>
2024-08-07 23:23:41 +0800

    feat: 密码加解密

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,31 +1,20 @@
-import { AES, enc, mode, pad } from 'crypto-js'
+import { AES, enc } from 'crypto-js'
 
 /** 密钥 与后端保持一致 */
-const key = enc.Utf8.parse('AD-PASSWORD-4567')
+const key = 'AD-PASSWORD-45678'
 
 // 解密
 export function Decrypt(word: string): string {
   if (!word)
     return word
 
-  const decrypt = AES.decrypt(word, key, {
-    iv: key,
-    mode: mode.CBC,
-    padding: pad.Pkcs7,
-  })
-
-  return decrypt.toString(enc.Utf8)
+  const bytes = AES.decrypt(word, key)
+  return bytes.toString(enc.Utf8)
 }
 
 // 加密
 export function Encrypt(word: string): string {
   if (!word)
     return word
-
-  const encrypted = AES.encrypt(word, key, {
-    iv: key,
-    mode: mode.CBC,
-    padding: pad.Pkcs7,
-  })
-  return encrypted.toString()
+  return AES.encrypt(word, key).toString()
 }
