e706b4f61f20ca3405e81cf302e7bc363e31f3fb
vaebe <18137693952@163.com>
2024-08-07 23:25:43 +0800

    feat(login): 模拟登录

---
+++ src/api/user.ts
@@ -0,0 +1,45 @@
+import Api from './base'
+import type { ResultData, ResultPageListData } from './base'
+import type { PaginationParameter } from '@/types'
+
+interface GetUserListParams extends PaginationParameter {
+  nickName: string
+  email: string
+}
+
+export interface UserInfo {
+  id?: string
+  email: string
+  avatar: string
+  nickName: string
+  password?: string
+  role: string
+  accountType: string
+  createdAt: string
+  updatedAt: string
+}
+
+// 获取用户列表
+export function getUserList(data: GetUserListParams): Promise<ResultPageListData<UserInfo[]>> {
+  return Api.post('/user/getList', data)
+}
+
+// 保存用户信息
+export function saveUserInfo(data: UserInfo): Promise<ResultData<{ id: string }>> {
+  return Api.post('/user/create', data)
+}
+
+// 更新用户信息
+export function updateUserInfo(data: UserInfo): Promise<ResultData<{ id: string }>> {
+  return Api.post('/user/update', data)
+}
+
+// 删除用户
+export function removeUser(params: { id: string }): Promise<ResultData<string>> {
+  return Api.delete('/user/delete', { params })
+}
+
+// 获取用户详情
+export function getUserDetails(params: { id: string }): Promise<ResultData<UserInfo>> {
+  return Api.get('/user/getUserInfo', { params })
+}
