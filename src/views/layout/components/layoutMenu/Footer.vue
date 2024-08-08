c57601e2a3d19694d6fda58c6f2b24f3c6720ad9
vaebe <18137693952@163.com>
2024-08-08 22:08:34 +0800

    feat(layout): 增加布局

---
+++ src/views/layout/components/layoutMenu/Footer.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { Expand, Fold } from '@element-plus/icons-vue'
+
+const layoutStore = useLayoutStore()
+const { menuCollapse } = storeToRefs(layoutStore)
+const { setMenuCollapse } = layoutStore
+</script>
+
+<template>
+  <div class="menu-footer">
+    <el-icon
+      v-if="menuCollapse === false"
+      class="cursor-pointer" size="36" color="black"
+      @click="setMenuCollapse(true)"
+    >
+      <Fold />
+    </el-icon>
+    <el-icon
+      v-else
+      class="cursor-pointer" size="36" color="black"
+      @click="setMenuCollapse(false)"
+    >
+      <Expand />
+    </el-icon>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.menu-footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 40px;
+  border-top: 1px solid #f5f5f5;
+  background: var(--layout-menu-header-bg);
+  transition: all 0.3s;
+}
+</style>
---
+++ src/views/layout/components/layoutMenu/Header.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const { menuCollapse } = storeToRefs(useLayoutStore())
+
+const router = useRouter()
+function jumpHome() {
+  router.push('/user')
+}
+</script>
+
+<template>
+  <div class="menu-header" @click="jumpHome">
+    <div class="menu-header__icon">
+      <img src="/logo.svg" alt="logo">
+    </div>
+    <p v-if="!menuCollapse" class="menu-header__title">
+      kk-admin
+    </p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.menu-header {
+  width: 100%;
+  height: 58px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: black;
+  background: var(--layout-menu-header-bg);
+  border-bottom: 1px solid #f5f5f5;
+
+  &__icon {
+    width: 38%;
+    height: 38px;
+  }
+
+  &__title {
+    margin-left: 14px;
+    font-size: 18px;
+    font-weight: 600;
+    text-align: center;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    cursor: pointer;
+  }
+}
+</style>
