5deaca18883354676bcea547ca046559f631955f
vae <18137693952@163.com>
2025-10-14 15:46:30 +0800

    feat: 更新脚本

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,8 +3,32 @@ import type { UserInfo } from '@/api/user'
 import dayjs from 'dayjs'
 import { http } from 'msw'
 import { Decrypt } from '@/utils/password'
-import { userList } from './data'
 import { getApiUrl, sendJson } from './utils'
+
+let userList: UserInfo[] = [
+  {
+    id: '1bofj153qd3188su7qb00u5n6oh',
+    email: 'admin@qq.com',
+    password: '123456',
+    nickName: 'kkf2Pg',
+    accountType: '01',
+    role: '01',
+    updatedAt: '2024-07-28 22:04:04',
+    createdAt: '2024-07-28 22:04:04',
+    avatar: 'https://api.dicebear.com/7.x/bottts-neutral/svg?seed=kkf2Pg&size=64',
+  },
+  {
+    id: '1n3o6n1qh7d2uywa45y8100xweg0w',
+    email: 'test@qq.com',
+    password: '123456',
+    nickName: 'kklpCj',
+    accountType: '01',
+    role: '01',
+    updatedAt: '2024-07-21 13:28:33',
+    createdAt: '2024-07-21 13:28:33',
+    avatar: 'https://api.dicebear.com/7.x/bottts-neutral/svg?seed=kklpCj&size=64',
+  },
+]
 
 const userAvatar = 'https://api.dicebear.com/7.x/bottts-neutral/svg?seed=kkf2Pg&size=64'
 
@@ -80,6 +104,8 @@ export const handlers = [
   http.delete(getApiUrl('api/user/:id'), ({ params }) => {
     const { id } = params
 
+    userList = userList.filter(user => user.id !== id)
+
     const user = userList.find(user => user.id === id)
 
     if (user) {
@@ -94,31 +120,29 @@ export const handlers = [
   http.post<RequireUserInfo, UserInfo>(getApiUrl('api/user/update'), async ({ request }) => {
     const newUserInfo = await request.json() as UserInfo
 
-    const user = userList.find(user => user.id === newUserInfo.id)
+    let isUserExists = false
 
-    if (user) {
+    userList = userList.map((user) => {
+      if (user.id === newUserInfo.id) {
+        isUserExists = true
+        return { ...newUserInfo, avatar: user.avatar }
+      }
+      return user
+    })
+
+    if (isUserExists) {
       return sendJson(0, '', '更新成功！')
     }
     else {
-      return sendJson(-1, null, 'User not found')
+      return sendJson(-1, null, '用户不存在！')
     }
   }),
 
   // 获取用户列表
   http.post(getApiUrl('api/user/getList'), () => {
-    const list: UserInfo[] = userList.map((user) => {
-      return {
-        ...user,
-        password: '',
-      }
-    })
+    const list: UserInfo[] = userList.map(user => ({ ...user, password: '' }))
 
-    const data = {
-      list,
-      pageNo: 1,
-      pageSize: 10,
-      total: list.length,
-    }
+    const data = { list, pageNo: 1, pageSize: 10, total: list.length }
 
     return sendJson(0, data)
   }),
