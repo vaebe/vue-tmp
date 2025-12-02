6f8d8467409a33c9facde0484a6486a146adacb0
vae <18137693952@163.com>
2025-12-02 17:54:15 +0800

    feat(utils): 优化 password.ts crypto-js 的导入

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,20 +1,23 @@
-import { AES, enc } from 'crypto-js'
+import AES from 'crypto-js/aes'
+import encUtf8 from 'crypto-js/enc-utf8'
 
 /** 密钥 与后端保持一致 */
 const key = 'AD-PASSWORD-45678'
 
 // 解密
 export function Decrypt(word: string): string {
-  if (!word)
+  if (!word) {
     return word
+  }
 
   const bytes = AES.decrypt(word, key)
-  return bytes.toString(enc.Utf8)
+  return bytes.toString(encUtf8)
 }
 
 // 加密
 export function Encrypt(word: string): string {
-  if (!word)
+  if (!word) {
     return word
+  }
   return AES.encrypt(word, key).toString()
 }
